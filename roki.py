47abf305126b2c50f3596a0daa1e4ec8ea399947
Roki-143-Cyber-King <133158815+Roki-143-Cyber-King@users.noreply.github.com>
2023-05-14 12:00:49 +0600

    Update roki.py

--- roki.py
+++ roki.py
@@ -4,7 +4,7 @@ os.system('clear')
 os.system("git pull")
 b = platform.architecture()[0]
 if b == '64bit':
-    __import__("PRI").menu()
+    __import__("PRO").menu()
     
 elif b == '32bit':
     print(" 32 BIT WILL AVAILABLE SOON ")
